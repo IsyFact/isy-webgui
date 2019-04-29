cfdfc9542f64c69f393996ce9bd58e09d290ecce
Manfred Roos <manfred.roos@msg.group>
2019-04-29 11:08:19 +0200

    IFS-206 TabContent Initialfuellung

---
+++ isy-webgui/src/main/java/de/bund/bva/pliscommon/pliswebgui/gui/jsfvorlagen/jsfsteuerelemente/JsfSteuerelementeAuskunftTabModel.java
@@ -0,0 +1,18 @@
+package de.bund.bva.pliscommon.pliswebgui.gui.jsfvorlagen.jsfsteuerelemente;
+
+import java.util.List;
+import com.google.common.collect.Lists;
+import de.bund.bva.isyfact.common.web.jsf.components.tab.TabGroupModel;
+
+/**
+ * Model für das Tab-Demobeispiel (Vollauskunft, Personalien, Sachverhalte)
+ *
+ */
+public class JsfSteuerelementeAuskunftTabModel extends TabGroupModel {
+
+    /**
+     * Die Serial-Version UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+}
