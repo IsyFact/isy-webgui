f30836a114625986616ee634d6330bcfd48b7e1a
Maike Glowik <maike.glowik@materna.de>
2020-01-13 14:44:06 +0100

    IFE-98 Konfigurationsumstellung auf Java (Awk-Wrapper)

--- isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/IsyWebguiApplication.java
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/IsyWebguiApplication.java
@@ -1,15 +1,44 @@
 package de.bund.bva.isyfact.isywebgui;
 
+import de.bund.bva.isyfact.aufrufkontext.AufrufKontext;
+import de.bund.bva.isyfact.aufrufkontext.AufrufKontextFactory;
+import de.bund.bva.isyfact.aufrufkontext.AufrufKontextVerwalter;
+import de.bund.bva.isyfact.aufrufkontext.stub.AufrufKontextVerwalterStub;
 import de.bund.bva.isyfact.isywebgui.config.XMLConfig;
 import de.bund.bva.isyfact.isywebgui.gui.config.GuiConfig;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import de.bund.bva.isyfact.sicherheit.Sicherheit;
+import de.bund.bva.isyfact.sicherheit.web.DelegatingAccessDecisionManager;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.AccessDecisionManager;
 
 @Configuration
 //@EnableAutoConfiguration
 @Import({ GuiConfig.class, XMLConfig.class })
 public class IsyWebguiApplication extends SpringBootServletInitializer {
+
+    @Profile("entwicklung")
+    @Bean
+    public AufrufKontextVerwalter<AufrufKontext> aufrufKontextVerwalter(
+        AufrufKontextFactory<AufrufKontext> factory) {
+        AufrufKontextVerwalterStub<AufrufKontext> verwalterStub = new AufrufKontextVerwalterStub<>();
+        verwalterStub.setRollen("Testrolle");
+        verwalterStub.setDurchfuehrendeBehoerde("123456");
+        verwalterStub.setDurchfuehrenderSachbearbeiterName("Max1 Mustermann");
+        verwalterStub.setDurchfuehrenderBenutzerKennung("max");
+        verwalterStub.setDurchfuehrenderBenutzerInterneKennung("max-01");
+        verwalterStub.setAufrufKontextFactory(factory);
+        verwalterStub.setFesterAufrufKontext(true);
+        return verwalterStub;
+    }
+
+    @Profile("entwicklung")
+    @Bean
+    public AccessDecisionManager accessDecisionManager(Sicherheit<AufrufKontext> sicherheit) {
+        return new DelegatingAccessDecisionManager(sicherheit);
+    }
 
 }
--- isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/gui/config/GuiConfig.java
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/gui/config/GuiConfig.java
@@ -2,6 +2,8 @@ package de.bund.bva.isyfact.isywebgui.gui.config;
 
 import de.bund.bva.isyfact.common.web.exception.common.AusnahmeIdMapper;
 import de.bund.bva.isyfact.common.web.exception.common.impl.AusnahmeIdMapperImpl;
+import de.bund.bva.isyfact.isywebgui.gui.flows.awkwrapper.FlowAwkWrapper;
+import de.bund.bva.isyfact.isywebgui.gui.flows.awkwrapper.impl.FlowAwkWrapperImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +16,21 @@ public class GuiConfig {
     /** Fallback-Ausnahme-ID, falls keine andere zugeordnet werden konnte. **/
     private static final String FALLBACK_AUSNAHME_ID = "WEBGI99999";
 
-//    @Bean
-//    public AusnahmeIdMapper ausnahmeIdMapper() {
-//        return new AusnahmeIdMapperImpl(FALLBACK_AUSNAHME_ID);
-//    }
+    /**
+     * Ausnahme ID - Mapper.
+     * Dieser Mapper wird auch automatisch für das IsyWeb Exception Handling verwendet
+     **/
+    @Bean
+    public AusnahmeIdMapper ausnahmeIdMapper() {
+        return new AusnahmeIdMapperImpl(FALLBACK_AUSNAHME_ID);
+    }
+
+    /**
+     * FlowAwkWrapper.
+     */
+    @Bean
+    public FlowAwkWrapper flowAwkWrapper() {
+        return new FlowAwkWrapperImpl();
+    }
+
 }
