bb1f94bbb70d29ce1c9810f99ec7cd736381a607
Maike Glowik <maike.glowik@materna.de>
2020-01-28 13:07:20 +0100

    IFE-98 Cleanup Config

--- isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/config/WebConfig.java
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/config/WebConfig.java
@@ -3,39 +3,15 @@ package de.bund.bva.isyfact.isywebgui.config;
 import ch.qos.logback.ext.spring.web.LogbackConfigListener;
 import de.bund.bva.isyfact.aufrufkontext.http.HttpHeaderNestedDiagnosticContextFilter;
 import de.bund.bva.isyfact.common.web.servlet.filter.ApplicationInitialisierungFilter;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.RequestContextFilter;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 public class WebConfig {
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
-    /**
-     * Register dispatcherServlet programmatically
-     *
-     * @return ServletRegistrationBean
-     */
-    @Bean
-    public ServletRegistrationBean dispatcherServletRegistration() {
-
-        ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet(), "/app/*");
-
-        registration
-            .setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-
-        return registration;
-    }
 
     @Bean
     public ServletContextInitializer servletContextInitializer() {
@@ -47,9 +23,18 @@ public class WebConfig {
             sc.setInitParameter("log4jRefreshInterval", "60000");
             sc.setInitParameter("javax.faces.FACELETS_BUFFER_SIZE", "202400");
             sc.setInitParameter("logbackConfigLocation", "classpath:/config/logback.xml");
-            sc.addFilter("RequestContextFilter", new RequestContextFilter());
             sc.addListener(LogbackConfigListener.class);
         };
+    }
+
+    //Request Scoping
+    @Bean
+    FilterRegistrationBean<RequestContextFilter> requestContextFilter() {
+        FilterRegistrationBean<RequestContextFilter> registrationBean =
+            new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestContextFilter());
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 
     @Bean
