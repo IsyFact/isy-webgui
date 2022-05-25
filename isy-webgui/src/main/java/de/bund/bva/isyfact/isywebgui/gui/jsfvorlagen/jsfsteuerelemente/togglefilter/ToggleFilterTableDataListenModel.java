12d71b7fa5ec6604d7dbbdddd482f24e598335f0
kaminm <maximilian.kaminski@msg.group>
2022-05-25 16:41:40 +0200

    feat: updated toggle filter demo  with more functionality

---
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/gui/jsfvorlagen/jsfsteuerelemente/togglefilter/ToggleFilterTableDataListenModel.java
@@ -0,0 +1,17 @@
+package de.bund.bva.isyfact.isywebgui.gui.jsfvorlagen.jsfsteuerelemente.togglefilter;
+
+import de.bund.bva.isyfact.common.web.jsf.components.datatable.DataTableModel;
+import de.bund.bva.isyfact.isywebgui.gui.jsfvorlagen.jsfsteuerelemente.datatable.JsfSteuerelementeTreffer;
+
+    /**
+     * Model für die Trefferliste.
+     *
+     */
+    public class ToggleFilterTableDataListenModel extends DataTableModel<ToggleFilterTableData> {
+
+        /**
+         * Die Serial-Version UID.
+         */
+        private static final long serialVersionUID = 1L;
+
+    }
--- isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/gui/jsfvorlagen/jsfsteuerelemente/togglefilter/TogglefilterBeispieleModel.java
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/gui/jsfvorlagen/jsfsteuerelemente/togglefilter/TogglefilterBeispieleModel.java
@@ -4,12 +4,40 @@ import java.util.List;
 import javax.faces.model.SelectItem;
 
 import de.bund.bva.isyfact.common.web.global.AbstractMaskenModel;
+import de.bund.bva.isyfact.common.web.jsf.components.datatable.DataTableInMemoryModel;
 import de.bund.bva.isyfact.common.web.jsf.components.panel.PanelModel;
 
 public class TogglefilterBeispieleModel extends AbstractMaskenModel {
 
     /** Panelmodel für Zustand des Panels. */
     private PanelModel panelModelToggleFilter = new PanelModel(true);
+
+    /**
+     * Model für Trefferliste (Client-Modus).
+     */
+    private ToggleFilterTableDataListenModel trefferlistenClientModel;
+
+    /**
+     * Model für Trefferliste (Server-Modus).
+     */
+    private DataTableInMemoryModel<ToggleFilterTableData> trefferlistenServerModel;
+
+    public ToggleFilterTableDataListenModel getTrefferlistenClientModel() {
+        return trefferlistenClientModel;
+    }
+
+    public void setTrefferlistenClientModel(ToggleFilterTableDataListenModel trefferlistenClientModel) {
+        this.trefferlistenClientModel = trefferlistenClientModel;
+    }
+
+    public DataTableInMemoryModel<ToggleFilterTableData> getTrefferlistenServerModel() {
+        return trefferlistenServerModel;
+    }
+
+    public void setTrefferlistenServerModel(
+            DataTableInMemoryModel<ToggleFilterTableData> trefferlistenServerModel) {
+        this.trefferlistenServerModel = trefferlistenServerModel;
+    }
 
     /**
      * Elemente für Toggle-Filter.
