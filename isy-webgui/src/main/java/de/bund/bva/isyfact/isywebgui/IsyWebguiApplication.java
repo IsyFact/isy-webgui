a303560263e69aa6d9534d78367bac9d286ca464
Roman Aklender (Materna) <roman.aklender@materna.group>
2023-09-18 17:40:11 +0200

    feat: ISY-344 modify configuration to use isy-security instead of isy-sicherheit

--- isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/IsyWebguiApplication.java
+++ isy-webgui/src/main/java/de/bund/bva/isyfact/isywebgui/IsyWebguiApplication.java
@@ -1,22 +1,18 @@
 package de.bund.bva.isyfact.isywebgui;
 
-import de.bund.bva.isyfact.isywebgui.servlet.requesthandler.ListpickerProviderRequestHandler;
-import de.bund.bva.isyfact.isywebgui.config.ResourceConfig;
-import de.bund.bva.isyfact.isywebgui.config.SicherheitConfig;
-import de.bund.bva.isyfact.isywebgui.config.WebConfig;
-import de.bund.bva.isyfact.isywebgui.gui.config.GuiConfig;
-import de.bund.bva.isyfact.isywebgui.gui.jsfvorlagen.jsfsteuerelemente.listpicker.JsfSteuerelementeListpickerController;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import de.bund.bva.isyfact.isywebgui.config.ResourceConfig;
+import de.bund.bva.isyfact.isywebgui.config.WebConfig;
+import de.bund.bva.isyfact.isywebgui.gui.config.GuiConfig;
 @Configuration
-//Securityconfig erfolgt über Isy-Sicherheit
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
-@Import({ GuiConfig.class, SicherheitConfig.class, ResourceConfig.class, WebConfig.class, })
+// Autoconfiguration of isy-security is used for security configuration
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+@Import({GuiConfig.class, ResourceConfig.class, WebConfig.class,})
 
 public class IsyWebguiApplication extends SpringBootServletInitializer {
 
